abcb630d76233f1ca023e9dee007f3aec419b41c
LokiDieKatze <sarah.marques.fr@gmail.com>
2021-02-16 14:06:04 +0100

    add test

---
+++ test1.js
@@ -0,0 +1,7 @@
+let msg = 'I have a love-hate relationship with Javascript.'
+console.log(`La longueur de msg est: ${msg.length}`)
+
+// Afficher chaque caractères d'une string et son index
+for (let i = 0; i < msg.length; ++i) {
+  console.log(`character ${msg[i]} is at index: ${i}`)
+}
